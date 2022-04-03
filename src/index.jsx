da3d603b566ed6b01c0d956cec479321c807f41e
Darko Lanovic <lanovic.darko@gmail.com>
2022-04-03 17:08:03 -0400

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -37,34 +37,3 @@ ReactDOM.render(React.createElement(MyFlixApplication), container);
 
 
 
-// NEW
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { MainView } from "./components/main-view/main-view";
-// import Container from "react-bootstrap/Container";
-
-// // Import statement to indicate that you need to bundle `./index.scss`
-// import "./index.scss";
-
-// // Main component (will eventually use all the others)
-// class MyFlixApplication extends React.Component {
-//   constructor() {
-//     super();
-//     // code executed right when the component is created in the memory
-//   }
-
-//   render() {
-//     return (
-//       <Container>
-//         <MainView />
-//       </Container>
-//     );
-//   }
-// }
-
-// // Finds the root of your app
-// const container = document.getElementsByClassName("app-container")[0];
-
-// // Tells React to render your app in the root DOM element
-// ReactDOM.render(React.createElement(MyFlixApplication), container);
